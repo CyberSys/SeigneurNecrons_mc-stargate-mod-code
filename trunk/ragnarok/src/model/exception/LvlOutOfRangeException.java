966d72cb2a92ca9afbe2277a69aa284bce70f5a4
lokisanada <lokisanada@1dab75a6-a841-452b-878d-d0c8fbd4df1c>
2012-09-04 00:49:14 +0000

    DESTROY!!!

--- trunk/ragnarok/src/model/exception/LvlOutOfRangeException.java
+++
@@ -1,9 +0,0 @@
-package model.exception;
-
-public class LvlOutOfRangeException extends Exception{
-	private static final long serialVersionUID = -3669373927415455582L;
-	
-	public LvlOutOfRangeException(int lvl){
-		super("Level " + lvl + " is illegal.");
-	}
-}
